f9e2bf3de8e7bacac760a528ea12bf5fb1408960
lpdswing <lpdswing@qq.com>
2024-07-31 12:49:14 +0000

    Deploying to pages from @ lpdswing/lpdswing.github.io@d4ac3e90b8bc909eb63bff24eb3615f101951141 🚀

---
+++ assets/technology_other_vitepress.md.DciaWX21.js
@@ -0,0 +1,45 @@
+import{_ as s,o as a,c as n,a6 as p}from"./chunks/framework.DFh5PHz-.js";const u=JSON.parse('{"title":"vitepress博客主题参数配置","description":"列举了一些常用参数。","frontmatter":{"title":"vitepress博客主题参数配置","description":"列举了一些常用参数。","comment":true,"tags":["vitepress"],"categories":["知识库"],"sticky":999},"headers":[],"relativePath":"technology/other/vitepress.md","filePath":"technology/other/vitepress.md","lastUpdated":1684467644000}'),e={name:"technology/other/vitepress.md"},l=p(`<h1 id="主题配置" tabindex="-1">主题配置 <a class="header-anchor" href="#主题配置" aria-label="Permalink to &quot;主题配置&quot;">​</a></h1><h2 id="home" tabindex="-1">Home <a class="header-anchor" href="#home" aria-label="Permalink to &quot;Home&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>---</span></span>
+<span class="line"><span>layout: home</span></span>
+<span class="line"><span># 首页部分元素定制</span></span>
+<span class="line"><span>blog:</span></span>
+<span class="line"><span> name: &#39;@lpdswing&#39;</span></span>
+<span class="line"><span> motto: 集中起来的意志，可以击穿顽石</span></span>
+<span class="line"><span> inspiring: 真正的大师永远都怀着一颗学徒的心</span></span>
+<span class="line"><span> pageSize: 6</span></span>
+<span class="line"><span>---</span></span></code></pre></div><h2 id="article" tabindex="-1">Article <a class="header-anchor" href="#article" aria-label="Permalink to &quot;Article&quot;">​</a></h2><h3 id="全部配置" tabindex="-1">全部配置 <a class="header-anchor" href="#全部配置" aria-label="Permalink to &quot;全部配置&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>---</span></span>
+<span class="line"><span>title: 文章标题（默认取一级标题）</span></span>
+<span class="line"><span>description: 类似副标题或者摘要（默认文章的前100字）</span></span>
+<span class="line"><span>cover: 首页卡片列表里的图片（默认文章的第一张图）</span></span>
+<span class="line"><span># hiddenCover为false则不会在文章页展示上述的封面</span></span>
+<span class="line"><span>hiddenCover: true </span></span>
+<span class="line"><span>#设置文章是否出现在首页列表</span></span>
+<span class="line"><span>hidden: false  </span></span>
+<span class="line"><span># 单独设置文章作者信息</span></span>
+<span class="line"><span>author： lpdswing  </span></span>
+<span class="line"><span># 预计阅读时间</span></span>
+<span class="line"><span>readingTime: true  </span></span>
+<span class="line"><span># 关闭评论</span></span>
+<span class="line"><span>comment: false  </span></span>
+<span class="line"><span>date: 2023-01-01</span></span>
+<span class="line"><span>tag: </span></span>
+<span class="line"><span> - go</span></span>
+<span class="line"><span>tags:</span></span>
+<span class="line"><span> - go</span></span>
+<span class="line"><span>categories:</span></span>
+<span class="line"><span> - 知识库</span></span>
+<span class="line"><span># 设置首页的精选文章，值越大越靠前</span></span>
+<span class="line"><span>sticky: 1  </span></span>
+<span class="line"><span># 用于设置在首页置顶展示的文章，从 1 开始，值越小越靠前</span></span>
+<span class="line"><span>top: 1  </span></span>
+<span class="line"><span># 用于设置文章左侧展示的 推荐文章 顺序（越小越靠前），或者在推荐列表中隐藏掉不展示</span></span>
+<span class="line"><span>recommend: 1</span></span>
+<span class="line"><span>---</span></span></code></pre></div><h3 id="默认常用配置" tabindex="-1">默认常用配置 <a class="header-anchor" href="#默认常用配置" aria-label="Permalink to &quot;默认常用配置&quot;">​</a></h3><p>写文章时可直接复制此配置</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>---</span></span>
+<span class="line"><span>title: </span></span>
+<span class="line"><span>comment: true  </span></span>
+<span class="line"><span>tags:</span></span>
+<span class="line"><span> - go</span></span>
+<span class="line"><span>categories:</span></span>
+<span class="line"><span> - 面试</span></span>
+<span class="line"><span># 设置首页的精选文章，值越大越靠前</span></span>
+<span class="line"><span>sticky: 1  </span></span>
+<span class="line"><span>---</span></span></code></pre></div>`,9),i=[l];function t(c,o,r,d,h,g){return a(),n("div",{"data-pagefind-body":!0},i)}const b=s(e,[["render",t]]);export{u as __pageData,b as default};
